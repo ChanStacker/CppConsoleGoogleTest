aef91337657133cdc13702159893bb8747370464
Chan Nundoo <chan.nundoo@gmail.com>
2024-09-22 22:26:31 +0100

    successfully tested logic from a console app by referencing the obj

--- GoogleTestStandAlone/test.cpp
+++ GoogleTestStandAlone/test.cpp
@@ -1,7 +1,7 @@
 #include "pch.h"
 #include "SutOne.h"
 #include "../SutStaticLib/SutStaticLib.h"
-#include "../SutConsoleApp/SutConsoleApp.h"
+#include "../SutConsoleApp/SutConsoleAppLogic.h"
 
 namespace googletest1 {
 	TEST(TestCaseName, TestName) {
@@ -29,13 +29,13 @@ namespace googleteststaticlib {
 	}
 }
 
-//namespace googletestconsoleapp {
-//
-//	TEST(SUTTestFix3, GetOutputTest)
-//	{
-//		std::string testString = "testString";
-//		int strCount = 10;
-//		int resultCount = GetConsoleOutput(testString);
-//		EXPECT_EQ(strCount, resultCount);
-//	}
-//}
+namespace googletestconsoleapp {
+
+	TEST(SUTTestFix3, GetOutputTest)
+	{
+		std::string testString = "testString";
+		int strCount = 10;
+		int resultCount = GetConsoleOutput(testString);
+		EXPECT_EQ(strCount, resultCount);
+	}
+}
