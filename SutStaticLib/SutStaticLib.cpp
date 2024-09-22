756b99dbe314e541c18ad6f639e72f069757a3ff
Chan Nundoo <chan.nundoo@gmail.com>
2024-09-22 22:03:32 +0100

    Add a static lib project

---
+++ SutStaticLib/SutStaticLib.cpp
@@ -0,0 +1,15 @@
+// SutStaticLib.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include <string>
+
+// TODO: This is an example of a library function
+void fnSutStaticLib()
+{
+}
+
+int GetStaticLibOutput(std::string input) {
+	return input.size();
+}
