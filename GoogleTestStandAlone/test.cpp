4799999266d9cfeef8228f59fd38f818065be4fa
Chan Nundoo <chan.nundoo@gmail.com>
2024-09-22 22:18:36 +0100

    add Console app breaks test discovery for google tests

--- GoogleTestStandAlone/test.cpp
+++ GoogleTestStandAlone/test.cpp
@@ -1,5 +1,7 @@
 #include "pch.h"
 #include "SutOne.h"
+#include "../SutStaticLib/SutStaticLib.h"
+#include "../SutConsoleApp/SutConsoleApp.h"
 
 namespace googletest1 {
 	TEST(TestCaseName, TestName) {
@@ -15,3 +17,25 @@ namespace googletest1 {
 		EXPECT_EQ(strCount, resultCount);
 	}
 }
+
+namespace googleteststaticlib {
+
+	TEST(SUTTestFix2, GetOutputTest)
+	{
+		std::string testString = "testString";
+		int strCount = 10;
+		int resultCount = GetStaticLibOutput(testString);
+		EXPECT_EQ(strCount, resultCount);
+	}
+}
+
+//namespace googletestconsoleapp {
+//
+//	TEST(SUTTestFix3, GetOutputTest)
+//	{
+//		std::string testString = "testString";
+//		int strCount = 10;
+//		int resultCount = GetConsoleOutput(testString);
+//		EXPECT_EQ(strCount, resultCount);
+//	}
+//}
---
+++ SutConsoleApp/SutConsoleAppLogic.cpp
@@ -0,0 +1,6 @@
+#include "SutConsoleAppLogic.h"
+
+int GetConsoleOutput(std::string input)
+{
+    return input.size();
+}
